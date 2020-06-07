bc41793b8e913f59034c9cd04eff81e200707c9d
Matthew Kevork <mkevork@ineat.ca>
2020-06-06 23:26:32 -0400

    finish section 60

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -3,20 +3,29 @@ console.log('client side javascript file is loaded')
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 
+const messageOne = document.querySelector('#message-one')
+const messageTwo = document.querySelector('#message-two')
+
+// messageOne.textContent = ""
+
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     
     const location = search.value
 
+    messageOne.textContent = "Loading weather information"
+
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error)
         {
-            console.log(data.error)
+            messageOne.textContent = data.error
+            messageTwo.textContent = ""
         }
         else {
-            console.log(data.location)
-            console.log(data.forecast)
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
         }
     })
 })
