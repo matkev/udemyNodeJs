de5d0d5273d2768582891b26862c42bdc189c9c5
Matthew Kevork <mkevork@ineat.ca>
2020-06-07 00:49:19 -0400

    finish section 60

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -6,29 +6,23 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-one')
 const messageTwo = document.querySelector('#message-two')
 
-// messageOne.textContent = ""
-
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    
+
     const location = search.value
 
     messageOne.textContent = "Loading weather information"
 
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
-    response.json().then((data) => {
-        if (data.error)
-        {
-            messageOne.textContent = data.error
-            messageTwo.textContent = ""
-        }
-        else {
-            messageOne.textContent = data.location
-            messageTwo.textContent = data.forecast
-        }
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+                messageTwo.textContent = ""
+            }
+            else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
     })
-})
-
-    console.log(location)
 })
