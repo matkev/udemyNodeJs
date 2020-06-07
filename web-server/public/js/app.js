fc78b32a20c3a00c48da104c1831233e9d4f52ce
Matthew Kevork <mkevork@ineat.ca>
2020-06-06 22:55:37 -0400

    finish section 58

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,6 +1,14 @@
 console.log('client side javascript file is loaded')
 
-fetch('http://localhost:3000/weather?address=Montreal').then((response) => {
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error)
         {
@@ -11,4 +19,7 @@ fetch('http://localhost:3000/weather?address=Montreal').then((response) => {
             console.log(data.forecast)
         }
     })
+})
+
+    console.log(location)
 })
