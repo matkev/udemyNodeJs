deebe9b74de2fd3c17ecc52d8dcfb62b1f122248
Matthew Kevork <mkevork@ineat.ca>
2020-08-28 16:55:59 -0400

    Finished section 95

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,14 +1,16 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        // resolve([7, 4, 1])
-        reject('This went all wrong')
-    }, 1000)
+const add= (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
 
+add(1, 2).then((sum) => {
+    console.log(sum)
+    return add(sum, 4)
+}).then((sum2) => {
+    console.log(sum2)
+}).catch((e) => {
+    console.log(e)
 })
-
-doWorkPromise.then((result) => {
-    console.log('Success', result)
-}).catch((error) => {
-    console.log('Error', error)
-})
-
